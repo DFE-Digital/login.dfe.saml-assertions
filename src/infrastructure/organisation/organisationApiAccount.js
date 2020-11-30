04dffe9930ed06e5e75efa3afd632856abcebfff
Sumanta Sarkar <sumanta.sr@gmail.com>
2020-11-30 09:54:25 +0000

    added DAO 1.0.41 for get Organsition (#87)

--- src/infrastructure/organisation/organisationApiAccount.js
+++ src/infrastructure/organisation/organisationApiAccount.js
@@ -1,49 +1,8 @@
-const jwtStrategy = require('login.dfe.jwt-strategies');
-const config = require('./../config');
-const rp = require('login.dfe.request-promise-retry');
 const { organisation } = require('login.dfe.dao');
 
-
-const callOrganisationApi = async (resource, body, method, reqId) => {
-  const token = await jwtStrategy(config.organisations.service).getBearerToken();
-  try {
-    const opts = {
-      method,
-      uri: `${config.organisations.service.url}/${resource}`,
-      headers: {
-        authorization: `bearer ${token}`,
-        'x-correlation-id': reqId,
-      },
-      json: true,
-    };
-    if (method === 'POST') {
-      opts.body = body;
-    }
-    const result = await rp(opts);
-
-    return {
-      success: true,
-      result,
-    };
-  } catch (e) {
-    return {
-      success: false,
-      statusCode: e.statusCode,
-      errorMessage: e.message,
-    };
-  }
-};
-
-
 const getOrganisationById = async (id, reqId) => {
-  const response = await callOrganisationApi(`organisations/v2/${id}`, null, 'GET', reqId);
-  if (!response.success) {
-    if (response.statusCode === 404) {
-      return null;
-    }
-    throw new Error(response.errorMessage);
-  }
-  return response.result;
+  const result = await organisation.getOrganisation(id);
+  return result;
 };
 
 const getOrganisationsForUser = async (userId, reqId) => {
